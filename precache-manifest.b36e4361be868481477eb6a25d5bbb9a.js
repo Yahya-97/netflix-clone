cf5609d3b86578acaffd58072757a960dba16b8b
unknown <yahyaiphone77@gmail.com>
2020-12-30 17:20:27 +0300

    Updates

---
+++ precache-manifest.b36e4361be868481477eb6a25d5bbb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2ec0b15804acbdb15565f3f2ebe1df",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "80cce8c5ee274bbb7728",
+    "url": "/netflix-clone/static/css/main.68da99a2.chunk.css"
+  },
+  {
+    "revision": "d66486b7c6603bfe6efd",
+    "url": "/netflix-clone/static/js/2.6e5b9175.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/netflix-clone/static/js/2.6e5b9175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cce8c5ee274bbb7728",
+    "url": "/netflix-clone/static/js/main.57635016.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
